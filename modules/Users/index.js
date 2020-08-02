fd295827510b8eb4d3f450bf94e26f429b3bc3dc
Victor Ernesto Martinez Leal <ficlel@Victors-MacBook-Air.local>
2020-08-02 04:13:08 +0200

    (feature, WIP) Added update address.

--- modules/Users/index.js
+++ modules/Users/index.js
@@ -66,6 +66,7 @@ const typeDefs = `
     addDriver(email: String!, name: String!, lastname: String!, dui: String!,address: String!, phone: String!): Person 
     updatePerson(id: ID!,name: String, email: String, password: String,phone:String): Person
     addAddressToPerson(person: ID,address: String,build: String,door:String): Address 
+    updateAddress(id: ID!, streetAddress: String, build: String, door: String, latitude: String, logintude: String): Address
     signPerson(person: NewUser): Person
     confirmUser(email: String): Person
     addCardToPerson(id: String,cardNumber: String,expMonth:Int,expYear:Int,cvc: Int,name: String): Cards
@@ -196,7 +197,7 @@ const resolvers = {
       	},
 	body:JSON.stringify({token:token})
       }).then((response) => {
-	      console.log(`Error: ${JSON.stringify(response)}`); 
+	      console.log(`Data: ${JSON.stringify(response)}`); 
 	      return response.json(); 
 	}).then((result)=>{
 	
@@ -329,6 +330,45 @@ const resolvers = {
         console.log(`error: ${error}`);
       });
       return response;
+    },
+    updateAddress: async (parent, args) => {
+      const session = driver.session();
+
+      let payload;
+
+      await session.run(
+        `
+        MATCH (n:address {id: $id})
+        WITH n
+        SET n.name = COALESCE(n.name, $name)
+        SET n.streetAddress = COALESCE(n.streetAddress, $streetAddress)
+        SET n.build = COALESCE(n.build, $build)
+        SET n.door = COALESCE(n.door, $door)
+        SET n.latitude = COALESCE(n.latitude, $latitude)
+        SET n.longitude = COALESCE(n.longitude, $longitude)
+
+        RETURN (n)
+        `,
+        {
+          id: args.id,
+          name: args.name,
+          streetAddress: args.streetAddress,
+          build: args.build,
+          door: args.door,
+          latitude: args.latitude,
+          longitude: args.longitude
+        }
+      ).then((result) => {
+        if (result.records.length > 0)
+          payload = result.records[0]._fields[0].properties;
+      })
+      .catch((e) => {
+        payload = null;
+
+        console.log(e);
+      });
+
+      return payload;
     },
     addAddressToPerson: async (parent, args)=>{
       let newAddress;
